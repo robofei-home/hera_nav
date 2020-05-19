26ff440d4c3c5aec785dc4a3f986feb5b2147544
Fagner Pimentel <fagnerpimentel@gmail.com>
2020-05-19 18:32:22 -0300

    add script to install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get install -y ros-melodic-move-base
+apt-get install -y ros-melodic-map-server
+apt-get install -y ros-melodic-amcl
+apt-get install -y ros-melodic-fake-localization
+apt-get install -y ros-melodic-carrot-planner
+apt-get install -y ros-melodic-global-planner
+apt-get install -y ros-melodic-dwa-local-planner
+apt-get install -y ros-melodic-eband-local-planner
+apt-get install -y ros-melodic-teb-local-planner
